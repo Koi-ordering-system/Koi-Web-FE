e3bb54835e7013407896eddb2d9ff53fa1afafa5
ATHT138 <anhthtservice@gmail.com>
2024-09-30 22:59:11 +0700

    Set up clerk service

--- src/views/home-page/homepage.tsx
+++ src/views/home-page/homepage.tsx
@@ -1,32 +1,32 @@
 import { ModeToggle } from "@/components/common";
-import { useUsers } from "@/hooks";
-import { useUserStore } from "@/stores/user-store";
-import { useEffect } from "react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  useAuth,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const HomePage = () => {
-  const { users, setUsers } = useUserStore();
-  const { data, error, isLoading } = useUsers();
+  const auth = useAuth();
 
-  useEffect(() => {
-    if (data) {
-      setUsers(data);
-    }
-  }, [data, setUsers]);
-
-  console.log(users);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+  auth.getToken().then((token) => {
+    console.log("Token: ", token);
+  });
 
   return (
     <div>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
       <h1 className="text-2xl">Home Page</h1>
       <p>Welcome </p>
+
       <ModeToggle />
     </div>
   );
