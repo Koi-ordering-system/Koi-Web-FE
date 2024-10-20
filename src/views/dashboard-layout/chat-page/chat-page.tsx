21b8e8e50820dbaa37893a9eb95ac77de732dd88
Cuong3103 <cuongpdse173196@fpt.edu.vn>
2024-10-20 17:03:30 +0700

    feat/chat-box-screen: Chat box screen

--- src/views/dashboard-layout/chat-page/chat-page.tsx
+++ src/views/dashboard-layout/chat-page/chat-page.tsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import WaittingRoom from "./components/room-list";
+import {HubConnectionBuilder,LogLevel} from "@microsoft/signalr"
+
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+const [connection,setConnection] = useState()
+
+
+const joinChatRoom = async (username:string,chatroom:string) =>{
+  try {
+    const conn = new HubConnectionBuilder()
+    .withUrl("https://koi-api.persiehomeserver.com/chat")
+    .configureLogging(LogLevel.Information)
+    .build()
+
+    conn.on("ReceiveMessage", (username: string, message: string) => {
+      console.log(`${username}: ${message}`);
+    });
+
+    await conn.start()
+    await conn.invoke("JoinRoom",{username,chatroom});
+    setConnection(conn)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+  return <div><WaittingRoom joinChatRoom={joinChatRoom}/></div>;
 };
 
 export default ChatPage;
